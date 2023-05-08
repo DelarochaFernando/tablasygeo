382a1d45ad4817132892a1e75f12105ccf5d2f3e
DelarochaFernando <ferchaparro11@hotmail.com>
2023-05-08 17:53:47 -0600

    - ProtoDataStore preferences API implementation leaving it pending. - Building next screen continue

--- app/src/main/java/com/delarochaf/tablasygeo/data/proto/UserPreferenceSerializer.kt
+++ app/src/main/java/com/delarochaf/tablasygeo/data/proto/UserPreferenceSerializer.kt
@@ -1,6 +1,6 @@
 package com.delarochaf.tablasygeo.data.proto
 
-import androidx.datastore.core.Serializer
+//import androidx.datastore.core.Serializer
 import java.io.InputStream
 import java.io.OutputStream
 
@@ -8,16 +8,16 @@ import java.io.OutputStream
 //https://medium.com/tech-takeaways/how-to-use-androids-proto-datastore-with-kotlin-support-8e4f80f1d6d
 
 
-object UserPreferenceSerializer : Serializer<UserPreferences>{
-    override val defaultValue: UserPreferences
-        get() = TODO("Not yet implemented")
-
-    override suspend fun readFrom(input: InputStream): UserPreferences {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun writeTo(t: UserPreferences, output: OutputStream) {
-        TODO("Not yet implemented")
-    }
-
-}
+//object UserPreferenceSerializer : Serializer<UserPreferences>{
+//    override val defaultValue: UserPreferences
+//        get() = TODO("Not yet implemented")
+//
+//    override suspend fun readFrom(input: InputStream): UserPreferences {
+//        TODO("Not yet implemented")
+//    }
+//
+//    override suspend fun writeTo(t: UserPreferences, output: OutputStream) {
+//        TODO("Not yet implemented")
+//    }
+//
+//}
