840ffba354bfd329102ab7740a510506fe7a62fa
DelarochaFernando <ferchaparro11@hotmail.com>
2023-05-18 17:54:12 -0600

    - MenuPrincipal, CardWithImageAndText composable implementation fixing Image showing properly with Glide library. PENDING - Try to fix blinking screen of the app - Research on how to implement different Screensizes UI adjustment.

--- app/src/main/java/com/delarochaf/tablasygeo/menuprincipal/MenuPrincipal.kt
+++ app/src/main/java/com/delarochaf/tablasygeo/menuprincipal/MenuPrincipal.kt
@@ -1,7 +1,11 @@
 package com.delarochaf.tablasygeo.menuprincipal
 
+import android.app.Activity
+import android.content.Context
 import android.content.res.Resources
 import android.graphics.BitmapFactory
+import android.graphics.drawable.ColorDrawable
+import android.graphics.drawable.Drawable
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -27,6 +31,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.currentCompositionLocalContext
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -35,9 +40,11 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
@@ -45,6 +52,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.graphics.drawable.toBitmap
 import androidx.navigation.NavController
 import com.delarochaf.tablasygeo.R
 import com.delarochaf.tablasygeo.ui.theme.TablasygeoTheme
@@ -89,9 +97,12 @@ fun MenuJuego(navController: NavController){
             routeText = "geographyScreen"
         )*/
         CardWithImageAndText(imageResId = R.drawable.menu_math_kids_logo1, title = "Matemáticas", description = "math")
+        Spacer(modifier = Modifier.height(24.dp))
         CardWithImageAndText(imageResId = R.drawable.menu_geography_kids_logo1, title = "Geografía", description = "geo")
     }
 
+    val activity = LocalContext.current as Activity
+    activity.window.statusBarColor = LocalContext.current.resources.getColor(R.color.green_chalkboard)
 }
 
 
@@ -182,16 +193,18 @@ fun CardWithImageAndText(imageResId: Int, title: String, description: String) {
     Card(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp),
+            .padding(horizontal = 150.dp),
         shape = RoundedCornerShape(8.dp),
         elevation = CardDefaults.cardElevation()
     ) {
         Column(
             modifier = Modifier.padding(16.dp)
         ) {
+
+            val resources = LocalContext.current.resources
             val bitmap by rememberSaveable{
                 mutableStateOf(BitmapFactory
-                    .decodeResource(Resources.getSystem(),imageResId))
+                    .decodeResource(resources,imageResId))
             }
             Image(
                 //painter = painterResource(id = imageResId),
@@ -199,13 +212,13 @@ fun CardWithImageAndText(imageResId: Int, title: String, description: String) {
                 bitmap = bitmap!!.asImageBitmap(),
                 contentDescription = null,
                 modifier = Modifier
-                    .height(180.dp)
+                    .height(260.dp)
                     .fillMaxWidth()
                     .clip(shape = RoundedCornerShape(8.dp)),
                 contentScale = ContentScale.Crop
             )
 
-            Spacer(modifier = Modifier.height(16.dp))
+            Spacer(modifier = Modifier.height(24.dp))
 
             Text(
                 text = title,
